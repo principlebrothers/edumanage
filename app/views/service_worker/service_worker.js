74fbe0c62addd7f55acdf415e644f35d0d89b08f
principlebrothers <principlesbrothers@yahoo.com>
2023-12-29 11:32:09 +0000

    Add service worker and manifest files

---
+++ app/views/service_worker/service_worker.js
@@ -0,0 +1,109 @@
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
+);
+
+const { NetworkFirst, CacheFirst, StaleWhileRevalidate } = workbox.strategies;
+const { registerRoute, setCatchHandler } = workbox.routing;
+const { warmStrategyCache } = workbox.recipes;
+const strategy = new CacheFirst();
+const urls = ['/offline.html'];
+warmStrategyCache({ urls, strategy });
+
+function onInstall(event) {
+  console.log('[Serviceworker]', 'Installing!', event);
+}
+
+function onActivate(event) {
+  console.log('[Serviceworker]', 'Activating!', event);
+}
+
+function onFetch(event) {
+  console.log('[Serviceworker]', 'Fetching!', event);
+}
+self.addEventListener('install', onInstall);
+self.addEventListener('activate', onActivate);
+self.addEventListener('fetch', onFetch);
+
+registerRoute(
+  ({ url }) => url.origin === 'https://fonts.googleapis.com',
+  new StaleWhileRevalidate({
+    cacheName: 'google-fonts-stylesheets',
+  })
+);
+
+registerRoute(
+  ({ request, url }) =>
+    request.destination === 'document' ||
+    (request.destination === '' &&
+      !url.pathname.includes('/network_test.png')),
+  new NetworkFirst({
+    cacheName: 'documents',
+  })
+);
+
+registerRoute(
+  ({ request }) =>
+    request.destination === 'script' || request.destination === 'style',
+  new CacheFirst({
+    cacheName: 'styles-and-scripts',
+  })
+);
+
+registerRoute(
+  ({ request }) => request.destination === 'images',
+  new CacheFirst({
+    cacheName: 'assets-images',
+  })
+);
+
+setCatchHandler(async ({ event }) => {
+  switch (event.request.destination) {
+    case 'document':
+      return strategy.handle({ event, request: urls[0] });
+    default:
+      return Response.error();
+  }
+});
+
+// const checkOnlineStatus = async () => {
+//   try {
+//     const online = await fetch('/network_test.png');
+//     return online.status >= 200 && online.status < 300;
+//   }catch (error) {
+//     return false;
+//   }
+// }
+
+// setInterval(async () => {
+//   const online = await checkOnlineStatus();
+//   if (online) {
+//     console.log('online');
+//   }
+//   else {
+//     console.log('offline');
+//   }
+// }, 5000);
+
+const checkOnlineStatus = async () => {
+  try {
+    // Ensure correct path construction
+    const url = new URL('/network_test.png', window.location.origin);
+
+    // Use 'HEAD' method for lightweight check
+    const response = await fetch(url, { method: 'HEAD' });
+    return response.ok;
+  } catch (error) {
+    return false;
+  }
+};
+
+setInterval(async () => {
+  const online = await checkOnlineStatus();
+  if (online) {
+    console.log('online');
+  } else {
+    console.log('offline');
+  }
+}, 5000);
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/service-worker.js', to: 'service_worker#service_worker'
+  get '/manifest.json', to: 'service_worker#manifest'
 end
