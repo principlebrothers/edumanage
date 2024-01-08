87598413e6f9d4a9835cb19c2deccfd24770b411
principlebrothers <principlesbrothers@yahoo.com>
2024-01-08 14:04:14 +0000

    Add new email templates and views for Devise mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
-  enum role: { user: 0, super_admin: 1, admin: 2, teacher: 3, guardian: 4, student: 5 }
+  enum role: { user: 0, teacher: 1, guardian: 2, student: 3 }
   after_initialize :set_default_role, if: :new_record?
 
   def set_default_role
