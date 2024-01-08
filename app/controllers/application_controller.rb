5cdd68446d4ed5efe121fecf401cc7d1c7c78e0c
principlebrothers <principlesbrothers@yahoo.com>
2024-01-08 13:05:41 +0000

    Add after_sign_in_path_for method to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    case resource
+    when User
+      root_path
+    when SuperAdmin
+      super_admin_dashboard_path
+    when Admin
+      admin_dashboard_path
+    end
+  end
 end
