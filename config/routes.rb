03c6978853ef9b609f963c51faa107e7dc71bc95
principlebrothers <principlesbrothers@yahoo.com>
2024-01-08 13:05:09 +0000

    Add Admin functionality and layout
    This commit adds the necessary files and code for the Admin functionality, including the AdminsController, Admin model, dashboard view, and admin layout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  devise_for :super_admins
+  devise_for :admins
   devise_for :users
+  get 'dashboard', to: 'super_admins#dashboard', as: :super_admin_dashboard
+  get 'admin_dashboard', to: 'admins#dashboard', as: :admin_dashboard
 
   get '/service-worker.js', to: 'service_worker#service_worker'
   get '/manifest.json', to: 'service_worker#manifest'
